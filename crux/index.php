0f4aa4224b2b25e7f860fce339b7128ced4952fd
ayushiaks <f2016587@hyderabad.ac.in>
2017-09-13 13:33:24 +0530

    add: config

---
+++ crux/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+include("database.php");
+
+ 
+$sql="SELECT * FROM fquestions ORDER BY id DESC";
+// OREDER BY id DESC is order result by descending
+ 
+$result=mysql_query($sql);
+?>
+ 
+<table width="90%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+	<tr>
+		<td width="6%" align="center" bgcolor="#E6E6E6"><strong>#</strong></td>
+		<td width="53%" align="center" bgcolor="#E6E6E6"><strong>Topic</strong></td>
+		<td width="15%" align="center" bgcolor="#E6E6E6"><strong>Views</strong></td>
+		<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Replies</strong></td>
+		<td width="13%" align="center" bgcolor="#E6E6E6"><strong>Date/Time</strong></td>
+	</tr>
+ 
+<?php
+ 
+// Start looping table row
+while($rows = mysql_fetch_array($result)){
+?>
+<tr>
+	<td bgcolor="#FFFFFF"><?php echo $rows['id']; ?></td>
+	<td bgcolor="#FFFFFF"><a href="view_topic.php?id=<?php echo $rows['id']; ?>"><?php echo $rows['topic']; ?></a><BR></td>
+	<td align="center" bgcolor="#FFFFFF"><?php echo $rows['view']; ?></td>
+	<td align="center" bgcolor="#FFFFFF"><?php echo $rows['reply']; ?></td>
+	<td align="center" bgcolor="#FFFFFF"><?php echo $rows['datetime']; ?></td>
+</tr>
+ 
+<?php
+// Exit looping and close connection 
+}
+mysql_close();
+?>
+ 
+<tr>
+	<td colspan="5" align="right" bgcolor="#E6E6E6"><a href="new_topic.php"><strong>Create New Topic</strong> </a></td>
+	</tr>
+</table>
